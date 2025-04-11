8d2672aceaa6f5bd0d81ed0c188470e66eb3901d
vebjornwn <vebjornwnilsen@outlook.com>
2025-04-11 16:17:15 -0700

    Updated number of workers to 4

--- pcnn_train.py
+++ pcnn_train.py
@@ -122,7 +122,7 @@ if __name__ == '__main__':
     #Reminder: if you have patience to read code line by line, you should notice this comment. here is the reason why we set num_workers to 0:
     #In order to avoid pickling errors with the dataset on different machines, we set num_workers to 0.
     #If you are using ubuntu/linux/colab, and find that loading data is too slow, you can set num_workers to 1 or even bigger.
-    kwargs = {'num_workers':0, 'pin_memory':True, 'drop_last':True}
+    kwargs = {'num_workers':4, 'pin_memory':True, 'drop_last':True}
 
     # set data
     if "mnist" in args.dataset:
