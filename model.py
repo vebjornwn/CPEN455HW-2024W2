b07709c9ead9db2bb7ce6f088d12724bde244fe4
vebjornwn <vebjornwnilsen@outlook.com>
2025-04-14 13:31:16 -0700

    Check

--- model.py
+++ model.py
@@ -152,10 +152,11 @@ class PixelCNN(nn.Module):
 
         if labels is not None:
             mid_class_embedding = self.mid_embedding(labels)  # shape: (batch_size, nr_filters)
-            mid_class_embedding = mid_class_embedding.view(x.size(0), self.nr_filters, 1, 1)
+            mid_class_embedding = mid_class_embedding.view(u.shape, self.nr_filters, 1, 1)
   
             u  = u_list.pop() + mid_class_embedding
             ul = ul_list.pop() + mid_class_embedding
+
 
         u  = u_list.pop()
         ul = ul_list.pop() 
