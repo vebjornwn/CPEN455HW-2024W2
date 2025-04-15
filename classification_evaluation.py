d376f421c7d82b2004508586b2d6197dc87c9a00
vebjornwn <vebjornwnilsen@outlook.com>
2025-04-15 11:44:58 -0700

    Now?

--- classification_evaluation.py
+++ classification_evaluation.py
@@ -34,9 +34,8 @@ def get_label(model, model_input, device):
 
     with torch.no_grad():
         for label in my_bidict.keys():
-            label_index = my_bidict[label]
-            label_list = [label_index] * image.size(0)
-            
+            label_list = [label] * image.size(0)
+
             outputs = model(image, labels=label_list)
             
             neg_log_likelihood = discretized_mix_logistic_loss(image, outputs)
